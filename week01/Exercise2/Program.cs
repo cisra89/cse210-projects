5fd03690665b1ae4508171e0969c10a3e6dfdcf5
cisra89 <icirnepercino@byupathway.edu>
2026-09-05 23:23:38 -0600

    w1e2complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,77 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your grade percentage?");
+            string gpercent = Console.ReadLine();
+
+            int xcent = int.Parse(gpercent);
+            string lgrade = "";
+            string sign = "";
+            int remainder = xcent % 10;
+
+        if (xcent >= 90)
+        {
+            lgrade = "A";
+
+        }
+        else if (xcent >= 80)
+        {
+            lgrade = "B";
+
+            if (remainder >= 7)
+            {
+                sign = "+";
+            }
+            else if (remainder <= 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (xcent >= 70)
+        {
+            lgrade = "C";
+
+            if (remainder >= 7)
+            {
+                sign = "+";
+            }
+            else if (remainder <= 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (xcent >= 60)
+        {
+            lgrade = "D";
+
+            if (remainder >= 7)
+            {
+                sign = "+";
+            }
+            else if (remainder <= 3)
+            {
+                sign = "-";
+            }
+        }
+            else
+            {
+                lgrade = "F";
+            }
+
+            Console.WriteLine($"Your grade is: {lgrade}{sign}");
+
+             if (xcent >= 70)
+            {
+                Console.WriteLine("Congrats! You passed!");
+            }
+            else
+            {
+                Console.WriteLine("You can do it better on the next time!");
+            }
+        }
     }
-}
