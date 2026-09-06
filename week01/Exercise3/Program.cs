adef15ed3c5ebc74b1db50fa4105d12ac329621f
cisra89 <icirnepercino@byupathway.edu>
2026-09-05 23:49:48 -0600

    w1e3complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Console.Write("Write your magic number ");
+            int magicNumber = int.Parse(Console.ReadLine());
+
+
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it! Congrats!");
+                }
+            }
+
+            Console.WriteLine($"You made {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
